450e1c2b91514d89ddaaddea1546887f8a6fe45c
Ken Krzeminski <krze@tumblr.com>
2015-08-27 21:52:29 -0400

    Part 1 complete. Tableview is generated with movie titles in each table cell

---
+++ rotten tomatoes/MoviesViewController.swift
@@ -0,0 +1,73 @@
+//
+//  MoviesViewController.swift
+//  rotten tomatoes
+//
+//  Created by iKreb Retina on 8/27/15.
+//  Copyright (c) 2015 krze. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tomatoesMoviesURL = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
+        let request = NSURLRequest(URL: tomatoesMoviesURL)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+            
+            if let json = json {
+                self.movies = json["movies"] as? [NSDictionary]
+                self.tableView.reloadData()
+            }
+        }
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+            
+        } else {
+            return 0
+        }
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let movie = movies![indexPath.row]
+        
+        cell.textLabel?.text = movie["title"] as? String
+        return cell
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
