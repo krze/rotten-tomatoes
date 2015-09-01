6e53d8b8e95722ee4032865559e02f7f8d3b4a81
Ken Krzeminski <krze@tumblr.com>
2015-08-31 20:34:02 -0400

    Hi-res images load asyncronously. Woo!

--- rotten tomatoes/MovieDetailsViewController.swift
+++ rotten tomatoes/MovieDetailsViewController.swift
@@ -22,8 +22,48 @@ class MovieDetailsViewController: UIViewController {
         titleLabel.text = movie["title"] as? String
         synopsisLabel.text = movie["synopsis"]  as? String
         
-        let url = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
-        backgroundImageView.setImageWithURL(url)
+//        let url = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+//        backgroundImageView.setImageWithURL(url)
+        
+        // Image downloading block
+        // Initial variables. Uncomment the gibberish thumbImageURL and comment the working thumbImageURL to test failure state.
+        //        let thumbImageURL = NSURL(string: "http://www.skjdhghdsljkfghdsljkfghsldkjfghieusr.com/dkfgjhsdkjfg.jpg")!
+        let thumbImageURL = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+        var hiResImageURL = NSURL(string: movie.valueForKeyPath("posters.original") as! String)!
+        
+        // hack to actually get the hiResImage
+        var hackhiResImageURL = "\(hiResImageURL)"
+        var range = hackhiResImageURL.rangeOfString(".*cloudfront.net/", options: .RegularExpressionSearch)
+        if let range = range {
+            hackhiResImageURL = hackhiResImageURL.stringByReplacingCharactersInRange(range, withString: "https://content6.flixster.com/")
+            hiResImageURL = NSURL(string: hackhiResImageURL)!
+        }
+        
+        let thumbImageURLRequest = NSURLRequest(URL: thumbImageURL)
+        let hiResImageURLRequest = NSURLRequest(URL: hiResImageURL)
+        
+        let placeholderImage = UIImage(named: "placeholder_image")
+        
+        // Start of the request. This will initially set the posterview with the existing placeholder.
+        backgroundImageView.setImageWithURLRequest(thumbImageURLRequest, placeholderImage: placeholderImage, success: { (request: NSURLRequest!, response: NSHTTPURLResponse!, image: UIImage!) -> Void in
+            // If the request for the thumbnail is successful, sets the downloaded image as the poster view. Holds the image in a variable for the subsequent request to download the hi-res image
+            let downloadedThumbImage = image
+            self.backgroundImageView.image = downloadedThumbImage
+            // Initiates another request to get the full image once the thumbnail is downloaded
+            self.backgroundImageView.setImageWithURLRequest(hiResImageURLRequest, placeholderImage: downloadedThumbImage, success: { (request: NSURLRequest!, response: NSHTTPURLResponse!, image: UIImage!) -> Void in
+                // If the request for the full image is successful, sets the downloaded full image as the poster view.
+                let downloadedHiResImage = image
+                self.backgroundImageView.image = downloadedHiResImage
+                }, failure: { (request: NSURLRequest!, response: NSHTTPURLResponse!, error: NSError!) -> Void in
+                    // If the request for the full image fails, it sets the poster image back as the cached thumbnail
+                    self.backgroundImageView.image = downloadedThumbImage
+            })
+            
+            }) { (request: NSURLRequest!, response: NSHTTPURLResponse!, error: NSError!) -> Void in
+                // If the request for the thumbnail fails, set it as the loading image placeholder
+                self.backgroundImageView.image = placeholderImage
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
--- rotten tomatoes/MoviesViewController.swift
+++ rotten tomatoes/MoviesViewController.swift
@@ -69,7 +69,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 }
             }
         }
-
+        
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -110,21 +110,44 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = movie["title"] as? String
         cell.synopsisLabel.text = movie["synopsis"] as? String
-        
-        let thumbImageURL = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+
+        // Image downloading block
+        // Initial variables. Uncomment the gibberish thumbImageURL and comment the working thumbImageURL to test failure state.
 //        let thumbImageURL = NSURL(string: "http://www.skjdhghdsljkfghdsljkfghsldkjfghieusr.com/dkfgjhsdkjfg.jpg")!
+        let thumbImageURL = NSURL(string: movie.valueForKeyPath("posters.thumbnail") as! String)!
+        var hiResImageURL = NSURL(string: movie.valueForKeyPath("posters.original") as! String)!
+        
+        // hack to actually get the hiResImage
+        var hackhiResImageURL = "\(hiResImageURL)"
+        var range = hackhiResImageURL.rangeOfString(".*cloudfront.net/", options: .RegularExpressionSearch)
+        if let range = range {
+            hackhiResImageURL = hackhiResImageURL.stringByReplacingCharactersInRange(range, withString: "https://content6.flixster.com/")
+            hiResImageURL = NSURL(string: hackhiResImageURL)!
+        }
+        
         let thumbImageURLRequest = NSURLRequest(URL: thumbImageURL)
-        let loadingImage = UIImage(named: "LoadingPhoto")
-//        cell.posterView.setImageWithURL(url)
-        cell.posterView.setImageWithURLRequest(thumbImageURLRequest, placeholderImage: loadingImage, success: { (request: NSURLRequest!, response: NSHTTPURLResponse!, image: UIImage!) -> Void in
-            cell.posterView.image = image
-            println("Success!")
-            println(response)
+        let hiResImageURLRequest = NSURLRequest(URL: hiResImageURL)
+        
+        let placeholderImage = UIImage(named: "placeholder_image")
+        
+        // Start of the request. This will initially set the posterview with the existing placeholder.
+        cell.posterView.setImageWithURLRequest(thumbImageURLRequest, placeholderImage: placeholderImage, success: { (request: NSURLRequest!, response: NSHTTPURLResponse!, image: UIImage!) -> Void in
+            // If the request for the thumbnail is successful, sets the downloaded image as the poster view. Holds the image in a variable for the subsequent request to download the hi-res image
+            let downloadedThumbImage = image
+            cell.posterView.image = downloadedThumbImage
+            // Initiates another request to get the full image once the thumbnail is downloaded
+            cell.posterView.setImageWithURLRequest(hiResImageURLRequest, placeholderImage: downloadedThumbImage, success: { (request: NSURLRequest!, response: NSHTTPURLResponse!, image: UIImage!) -> Void in
+                // If the request for the full image is successful, sets the downloaded full image as the poster view.
+                let downloadedHiResImage = image
+                cell.posterView.image = downloadedHiResImage
+                }, failure: { (request: NSURLRequest!, response: NSHTTPURLResponse!, error: NSError!) -> Void in
+                // If the request for the full image fails, it sets the poster image back as the cached thumbnail
+                cell.posterView.image = downloadedThumbImage
+            })
+            
             }) { (request: NSURLRequest!, response: NSHTTPURLResponse!, error: NSError!) -> Void in
-            cell.posterView.image = loadingImage
-            println("Failure!!")
-            println("\(error)\n")
-            println(response)
+            // If the request for the thumbnail fails, set it as the loading image placeholder
+            cell.posterView.image = placeholderImage
         }
         
         return cell
